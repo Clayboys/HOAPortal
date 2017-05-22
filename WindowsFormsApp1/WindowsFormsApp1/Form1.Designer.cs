6dd47d207ffabcd3ec60662a57c5e00aa7438c9d
Clayboys <042hansonp@gmail.com>
2017-05-22 15:27:26 -0500

    Add files via upload
    first commit

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(496, 481);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Gps Coordinates";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(12, 54);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(396, 351);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.PictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(430, 54);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(396, 351);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // gMapControl1
+            // 
+            this.gMapControl1.Bearing = 0F;
+            this.gMapControl1.CanDragMap = true;
+            this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapControl1.GrayScaleMode = false;
+            this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapControl1.LevelsKeepInMemmory = 5;
+            this.gMapControl1.Location = new System.Drawing.Point(340, 411);
+            this.gMapControl1.MarkersEnabled = true;
+            this.gMapControl1.MaxZoom = 18;
+            this.gMapControl1.MinZoom = 2;
+            this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapControl1.Name = "gMapControl1";
+            this.gMapControl1.NegativeMode = false;
+            this.gMapControl1.PolygonsEnabled = true;
+            this.gMapControl1.RetryLoadTile = 0;
+            this.gMapControl1.RoutesEnabled = true;
+            this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapControl1.ShowTileGridLines = false;
+            this.gMapControl1.Size = new System.Drawing.Size(150, 150);
+            this.gMapControl1.TabIndex = 6;
+            this.gMapControl1.Zoom = 13D;
+            this.gMapControl1.Load += new System.EventHandler(this.GMapControl1_Load);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(125, 471);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 54);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "start capturing";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "169.254.17.23",
+            "169.254.180.49",
+            "169.254.131.67",
+            "169.254.19.136"});
+            this.comboBox1.Location = new System.Drawing.Point(544, 24);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(267, 24);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "169.254.17.23",
+            "169.254.180.49",
+            "169.254.131.67",
+            "169.254.19.136"});
+            this.comboBox2.Location = new System.Drawing.Point(125, 24);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(267, 24);
+            this.comboBox2.TabIndex = 9;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.ComboBox2_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Stream 1 IP:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(453, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Stream 2 IP:";
+            this.label3.Click += new System.EventHandler(this.Label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(838, 560);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.gMapControl1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private GMap.NET.WindowsForms.GMapControl gMapControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,220 @@
+﻿using AForge.Video;
+using MjpegProcessor;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Gmap.net;
+using System.Threading;
+using System.IO;
+using XperiCode.JpegMetadata;
+using System.Drawing.Imaging;
+using System.Text;
+
+namespace WindowsFormsApp1{
+    public partial class Form1 : Form{
+
+        //private System.IO.Ports.SerialPort port;
+        private NmeaParser.SerialPortDevice device;
+        private static MJPEGStream stream1 = new MJPEGStream() { Source = "" };
+        private static MJPEGStream stream2 = new MJPEGStream() { Source = "" };
+        private NmeaParser.Nmea.Gps.Gprmc gprmc;
+        private delegate void StringArgReturningVoidDelegate();
+        System.Collections.Generic.List<object> clients = new System.Collections.Generic.List<object>();
+        private string[] ipAddresses = { "169.254.17.23", "169.254.180.49", "169.254.131.67", "169.254.19.136" };
+        bool isCapturing = false;
+        System.Threading.Timer timer;
+
+        public Form1(){
+            InitializeComponent();
+            device = new NmeaParser.SerialPortDevice(new System.IO.Ports.SerialPort("COM3", 4800));
+            device.MessageReceived += Device_MessageReceived; ;
+            device.OpenAsync();
+            stream1.NewFrame += Stream1_NewFrame;
+            stream2.NewFrame += Stream2_NewFrame;
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 1;
+            stream1.Start();
+            stream2.Start();
+            UpdateStreamSources();
+            button1.BackColor = Color.Green;
+            }
+        private void UpdateStreamSources(){
+            stream1.Stop();
+            stream2.Stop();
+            stream1.Source = "http://" + this.ipAddresses[comboBox1.SelectedIndex] + ":8080/?action=stream";
+            stream2.Source = "http://" + this.ipAddresses[comboBox2.SelectedIndex] + ":8080/?action=stream";
+            stream1.Start();
+            stream2.Start();
+            }
+        private void Stream2_NewFrame(object sender, NewFrameEventArgs e){
+            try{
+                pictureBox2.Image = new Bitmap(e.Frame);
+                }
+            catch (Exception ex){
+                Console.WriteLine(ex.Message);
+                }
+            }
+        private void Stream1_NewFrame(object sender, NewFrameEventArgs e){
+            try{
+                pictureBox1.Image = new Bitmap(e.Frame);
+                }
+            catch (Exception ex){
+                Console.WriteLine(ex.Message);
+                }
+            }
+        private void Device_MessageReceived(object sender, NmeaParser.NmeaMessageReceivedEventArgs e){
+            if (e.Message is NmeaParser.Nmea.Gps.Gprmc){
+                gprmc = e.Message as NmeaParser.Nmea.Gps.Gprmc;
+                //try {
+                //    timer.Change((int)(1000.0/(gprmc.Speed/0.514444)), (int)(1000.0 / (gprmc.Speed / 0.514444)));
+                //    }
+                //catch (System.Exception ex) { Console.WriteLine(ex.Message); }
+                try
+                {
+                    new Thread(new ThreadStart(ThreadSafeUpdateLabel1)).Start();
+                    }
+                catch (System.Exception ex){Console.WriteLine(ex.Message);}
+                try{
+                    new Thread(new ThreadStart(ThreadSafeUpdateGmap1)).Start();
+                    }
+                catch (System.Exception ex){Console.WriteLine(ex.Message);}
+                }
+            }
+        private void ThreadSafeUpdateGmap1() {
+            try{
+                if (this.gMapControl1.InvokeRequired){
+                    StringArgReturningVoidDelegate d = new StringArgReturningVoidDelegate(ThreadSafeUpdateGmap1);
+                    this.Invoke(d);
+                    }
+                else{
+                    gMapControl1.Position = new GMap.NET.PointLatLng(gprmc.Latitude, gprmc.Longitude);
+                    }
+                }
+            catch (System.Exception ex) { Console.WriteLine(ex.Message); }
+            }
+        private void ThreadSafeUpdateLabel1(){
+            try{
+                if (this.label1.InvokeRequired){
+                    StringArgReturningVoidDelegate d = new StringArgReturningVoidDelegate(ThreadSafeUpdateLabel1);
+                    this.Invoke(d);
+                    }
+                else{
+                    this.label1.Text = $"{gprmc.Latitude},{gprmc.Longitude},{gprmc.Speed / 0.514444} m/s"; this.gMapControl1.Position = new GMap.NET.PointLatLng(gprmc.Latitude, gprmc.Longitude);
+                    }
+                }
+            catch (System.Exception ex) { Console.WriteLine(ex.Message); }
+            }
+        private void GMapControl1_Load(object sender, EventArgs e){
+            gMapControl1.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance;
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
+            }
+        private void ThreadSafeGetImage(object args){
+            Thread tempThread = new Thread(new ThreadStart(ThreadSafeGetImage1));
+            tempThread.Start();
+            }
+        private void ThreadSafeGetImage1(){
+            GetImage(new { });
+            }
+        private void GetImage(object args) {
+            try{
+                if (this.gMapControl1.InvokeRequired){
+                    StringArgReturningVoidDelegate d = new StringArgReturningVoidDelegate(ThreadSafeGetImage1);
+                    this.Invoke(d);
+                    }
+                else{
+                    try{
+                        //Console.Write("trying, ");
+                        using (System.Net.WebClient client = new System.Net.WebClient()){
+                        ///*
+                            client.DownloadFile("http://" + ipAddresses[comboBox1.SelectedIndex] + ":8080/?action=snapshot", @"C:\Users\Paul\Desktop\pics\(Cam1)(" + Math.Floor((DateTime.Now.ToUniversalTime() - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds) + ")(" + gprmc.Latitude.ToString() + ", " + gprmc.Longitude.ToString() + ").jpg");
+                            client.DownloadFile("http://" + ipAddresses[comboBox2.SelectedIndex] + ":8080/?action=snapshot", @"C:\Users\Paul\Desktop\pics\(Cam2)(" + Math.Floor((DateTime.Now.ToUniversalTime() - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds) + ")(" + gprmc.Latitude.ToString() + ", " + gprmc.Longitude.ToString() + ").jpg");
+                            //WriteExif();
+                            if ((int)(1000.0 / (gprmc.Speed / 0.514444)) > 2000) { Thread.Sleep(2000); }
+                            else { Thread.Sleep((int)(1000.0 / (gprmc.Speed / 0.514444))); }
+                            }
+                        }
+                    catch {
+                        try{
+                            using (System.Net.WebClient client = new System.Net.WebClient()){
+                                //*/
+                                client.DownloadFile("http://" + ipAddresses[comboBox1.SelectedIndex] + ":8080/?action=snapshot", @"C:\Users\Paul\Desktop\pics\(Cam1)(" + Math.Floor((DateTime.Now.ToUniversalTime() - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds) + ")(no gps).jpg");
+                                client.DownloadFile("http://" + ipAddresses[comboBox2.SelectedIndex] + ":8080/?action=snapshot", @"C:\Users\Paul\Desktop\pics\(Cam2)(" + Math.Floor((DateTime.Now.ToUniversalTime() - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds) + ")(no gps).jpg");
+                                //Thread.Sleep(333);
+                                }
+                            }
+                        catch (System.Exception ex) { throw ex; }
+                        }//catch (System.Exception ex) { throw ex; }
+                    }
+                }
+            catch (System.Exception ex) {
+                isCapturing = false;
+                timer.Change(-1, -1);
+                timer.Dispose();
+                button1.Text = "start capturing";
+                button1.BackColor = Color.Green;
+                MessageBox.Show("No camera feed, bruh", "bruh", MessageBoxButtons.OK); 
+                }
+            }
+        private void Button1_Click(object sender, EventArgs e){
+            if (isCapturing){ 
+                isCapturing = false;
+                timer.Change(-1, -1);
+                timer.Dispose();
+                button1.Text = "start capturing";
+                button1.BackColor = Color.Green;
+                }
+            else{
+                try{
+                    object obj = new { gprmc.Latitude };
+                    }
+                catch { MessageBox.Show("No gps, bruh", "bruh", MessageBoxButtons.OK); }
+                try{
+                    isCapturing = true;
+                    timer = new System.Threading.Timer(GetImage, null, TimeSpan.FromSeconds(0.333), TimeSpan.FromSeconds(0.333));
+                    //Thread.Sleep(4500);
+                    button1.Text = "stop capturing";
+                    button1.BackColor = Color.Red;
+                    }
+                catch (System.Exception ex) { throw ex; }
+                }
+            }
+        /*public void WriteExif(){
+            var image1 = Image.FromFile("C:\\Users\\Paul\\Desktop\\pics1\\Cam3image" + imageCount1.ToString() + ".jpg");
+            PropertyItem propitem1 = image1.PropertyItems[0];
+            propitem1.Id= 0x9286;
+            propitem1.Type = 2;
+            propitem1.Value = Encoding.ASCII.GetBytes(gprmc.Latitude.ToString() + ", " + gprmc.Longitude.ToString());
+            propitem1.Len = propitem1.Value.Length;
+            image1.SetPropertyItem(propitem1);
+            System.IO.File.Delete("C:\\Users\\Paul\\Desktop\\pics\\Cam1image" + imageCount1.ToString() + ".jpg");
+            image1.Save("C:\\Users\\Paul\\Desktop\\pics\\Cam1Image"+imageCount2.ToString()+".jpg");
+
+            image1 = Image.FromFile("C:\\Users\\Paul\\Desktop\\pics\\Cam4image" + imageCount2.ToString() + ".jpg");
+            propitem1 = image1.PropertyItems[0];
+            propitem1.Id = 0x9286;
+            propitem1.Type = 2;
+            propitem1.Value = Encoding.ASCII.GetBytes(gprmc.Latitude.ToString() + ", " + gprmc.Longitude.ToString());
+            propitem1.Len = propitem1.Value.Length;
+            image1.SetPropertyItem(propitem1);
+            System.IO.File.Delete("C:\\Users\\Paul\\Desktop\\pics\\Cam2image" + imageCount2.ToString() + ".jpg");
+            image1.Save("C:\\Users\\Paul\\Desktop\\pics\\Cam2image" + imageCount2.ToString() + ".jpg");
+                
+            //Image image2 = Image.FromFile("C:\\Users\\Paul\\Desktop\\pics\\Cam2image" + imageCount2.ToString() + ".jpg");
+            //PropertyItem propitem2 = image2.GetPropertyItem(37510);
+            //propitem2.Value = Encoding.ASCII.GetBytes(gprmc.Latitude.ToString() + ", " + gprmc.Longitude.ToString());
+            //image2.SetPropertyItem(propitem2);
+            }*/
+        protected override void OnFormClosing(FormClosingEventArgs e){
+            base.OnFormClosing(e);
+            if (e.CloseReason == CloseReason.WindowsShutDown) return;
+            Environment.Exit(0);
+            }
+        private void ComboBox2_SelectedIndexChanged(object sender, EventArgs e){try{UpdateStreamSources();}catch { }}
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e){try{UpdateStreamSources();}catch { }}
+        private void Form1_Load(object sender, EventArgs e){}
+        private void PictureBox1_Click(object sender, EventArgs e){ }
+        private void PictureBox2_Click(object sender, EventArgs e) { }
+        private void Label1_Click(object sender, EventArgs e) { }
+        private void Label3_Click(object sender, EventArgs e){}
+        }
+    }
